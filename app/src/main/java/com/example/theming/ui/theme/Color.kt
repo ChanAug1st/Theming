159e9bb94893df25dd5af287e87e1feb0d4d38d7
ChanAug1st <ChAugustchanjuan@outlook.com>
2025-02-02 17:36:15 +0200

    Initial commit

---
+++ app/src/main/java/com/example/theming/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.example.theming.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val OuasOrange = Color(0xFFF7921E)
+
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val PurpleGrey40 = Color(0xFF625B71)
+val Pink80 = Color(0xFFEFB8C8)
+val Pink40 = Color(0xFF7D5260)
